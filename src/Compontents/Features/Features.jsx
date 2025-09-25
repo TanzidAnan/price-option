e07f6f665bdaed8ccf0fb7e99c21b57127000d26
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-26 01:05:15 +0600

    grow

--- src/Compontents/Features/Features.jsx
+++ src/Compontents/Features/Features.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Features = ({fea}) => {
     return (
         <div>
-            <p># {fea}</p>
+            <p className='py-2'># {fea}</p>
         </div>
     );
 };
--- src/Compontents/PriceOption/PriceOption.jsx
+++ src/Compontents/PriceOption/PriceOption.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import Features from '../Features/Features';
 
-const PriceOption = ({option}) => {
-    const {name,price,features} =option
+const PriceOption = ({ option }) => {
+    const { name, price, features, image } = option
     return (
-        <div className='bg-amber-800 rounded-2xl p-4 text-white text-center'>
-            <h2>
+        <div className='bg-amber-800 rounded-2xl p-4 text-white flex flex-col '>
+            <h2 className=' text-center'>
                 <span className='text-4xl font-extrabold'>{price}</span>
                 <span>/mon</span>
             </h2>
-            <h4 className='text-2xl my-4'>{name}</h4>
-            {
-                features.map(fea => <Features fea={fea}></Features>)
-            }
+            {/* <img className='w-20' src={image} alt="gym" /> */}
+            <h4 className='text-2xl my-4  text-center'>{name}</h4>
+            <div className='flex-grow '>
+                {
+                    features.map((fea, idx) => <Features key={idx} fea={fea}></Features>)
+                }
+            </div>
+            <button className='mt-12 bg-blue-800 w-full py-2 font-bold rounded-3xl'>Buy Now</button>
         </div>
     );
 };
